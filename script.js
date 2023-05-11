faaa7654274485126ff6f4754964931c85824988
AlbertoJL3 <363alberto@gmail.com>
2023-05-11 15:53:01 -0400

    added URL to contact card

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var testbox = document.querySelector('.test');
 
 testbox.addEventListener('click', function() {
-    window.location.href = 'google.com'
+    window.location.href = 'https://www.linkedin.com/in/AlbertoJL3';
 });
